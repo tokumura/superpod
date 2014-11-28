a740d0ede9a6420711d5fb178dd322995e858433
Tsuyoshi Okumura <qkrhn081@gmail.com>
2014-11-29 01:48:02 +0900

    fix

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,12 @@
 class WelcomeController < ApplicationController
   before_filter :authenticate_user!
   def index
+    if HostConfig.all.size > 0
+      @move_to = images_path
+    else
+      @move_to = host_configs_path
+    end
+    
     redirect_to images_path
   end
 end
